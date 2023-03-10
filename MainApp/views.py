07c78582965dd7d1260eaeda1a17b22d46d3407a
Second2k <125477193+Second2k@users.noreply.github.com>
2023-03-10 20:05:06 +0300

    Add files via upload

---
+++ MainApp/views.py
@@ -0,0 +1,39 @@
+import json
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseNotFound
+from django.core.exceptions import ObjectDoesNotExist
+from MainApp.models import Country, Language
+
+
+def home(request):
+    context = {
+        "name": "Alex",
+        "email": "Homework@django.com",
+    }
+    return render(request, "home.html", context)
+
+
+def get_country(request,name):
+    try:
+        country = Country.objects.get(country=name)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f"Country with name={name} not found")
+    context = {
+        'country': country.country,
+        'languages': ', '.join(list(country.languages.all().values_list('name', flat=True))),
+    }
+    return render(request, 'country.html', context)
+
+
+def get_countries_list(request):
+    context = {
+        "countries_list": Country.objects.all(),
+    }
+    return render(request, "countries_list.html", context)
+    
+    
+def get_languages_list(request):
+    context = {
+        "languages_list": Language.objects.all(),
+    }
+    return render(request, "languages_list.html", context)
